6cdf70653c53b512ab1b02f6174875652856cd1a
nupur <nupurbaghel@gmail.com>
2019-07-11 19:06:32 +0530

    adding solutions for 2-D histogram exercise

--- 4_3_2D_Histogram_Filter/localizer.py
+++ 4_3_2D_Histogram_Filter/localizer.py
@@ -1,4 +1,4 @@
-# import pdb
+import pdb
 from helpers import normalize, blur
 
 def initialize_beliefs(grid):
@@ -16,11 +16,19 @@ def initialize_beliefs(grid):
 
 def sense(color, grid, beliefs, p_hit, p_miss):
     new_beliefs = []
-
-    #
     # TODO - implement this in part 2
-    #
-
+    height = len(grid)
+    width = len(grid[0])
+    for i in range(height):
+        row = []
+        for j in range(width):
+            hit = (grid[i][j]==color)
+            row.append(beliefs[i][j]*(hit*p_hit + (1-hit)*p_miss))
+        new_beliefs.append(row)
+    s = sum(sum(new_beliefs,[]))
+    for i in range(height):
+        for j in range(width):
+            new_beliefs[i][j] = new_beliefs[i][j]/s
     return new_beliefs
 
 def move(dy, dx, beliefs, blurring):
@@ -29,8 +37,8 @@ def move(dy, dx, beliefs, blurring):
     new_G = [[0.0 for i in range(width)] for j in range(height)]
     for i, row in enumerate(beliefs):
         for j, cell in enumerate(row):
-            new_i = (i + dy ) % width
-            new_j = (j + dx ) % height
-            # pdb.set_trace()
+            new_i = (i + dy ) % height
+            new_j = (j + dx ) % width
+#             pdb.set_trace()
             new_G[int(new_i)][int(new_j)] = cell
     return blur(new_G, blurring)
